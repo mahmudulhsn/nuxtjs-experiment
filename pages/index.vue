d7e671b2ddf5321a2d334bba53d2218995167703
Mahmudul Hasan Hasib <hasib.mahmudul@gmail.com>
2021-04-16 23:41:57 +0600

    Create a simple project with nuxt js

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largecardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
+    <SmallCardDisplay
+      v-for="cardInfo in smallcardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections,  smallCardSections } from "@/assets/data.js";
+export default {
+  data () {
+    return {
+      largecardInfo: largeCardSections,
+      smallcardInfo: smallCardSections
+    }
+  }
+}
+</script>
