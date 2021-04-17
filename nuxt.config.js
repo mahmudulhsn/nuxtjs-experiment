f29dae485482acc492dac9ea5aa5eafaddfb9874
Mahmudul Hasan Hasib <hasib.mahmudul@gmail.com>
2021-04-17 17:53:29 +0600

    render single product

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'myapp',
+    title: 'NuxtJS Experiment',
     htmlAttrs: {
       lang: 'en'
     },
